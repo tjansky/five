3c3a39657392c0a9f4c3b7d54ae1b0e4b5cdf120
tjansky <tomislav.jansky@gmail.com>
2022-05-30 22:09:12 +0200

    nav malo postelan

--- newsy/Newsy/Client/src/app/layout/nav/nav.component.ts
+++ newsy/Newsy/Client/src/app/layout/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { Author } from 'src/app/data/schema/Author';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  loggedUser!: Author;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { 
+    this.authService.currentUser.subscribe(x => this.loggedUser = x);
+  }
 
   ngOnInit(): void {
+  }
+
+  onLogIn() {
+    this.router.navigate(['/auth/login']);
+  }
+
+  onLogOut() {
+    this.authService.logout();
   }
 
 }
